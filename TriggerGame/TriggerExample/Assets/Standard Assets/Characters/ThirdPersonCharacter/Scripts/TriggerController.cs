3246eb968af15082a4842afb4612b7f8a0d3fc03
misiek1992 <michel.wojciech@gmail.com>
2017-06-06 17:52:17 +0100

    TriggerGame
    ktores labki, 2 albo 4

---
+++ TriggerGame/TriggerExample/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/TriggerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerController : MonoBehaviour {
+    
+    int tmp;
+    private void Update()
+    {
+        if (tmp==1)
+        {
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            tmp = 1;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            tmp = 0;
+        }
+    }
+}

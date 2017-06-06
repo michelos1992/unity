ddb3690b5e32001602e76297d47d8ef9a8eba5f6
misiek1992 <michel.wojciech@gmail.com>
2017-06-06 16:16:24 +0100

    2lab

---
+++ 2DPlatformer/2DPlatformerYt/Assets/Script/DestroyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    void OnCollision2D(GameObject other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            Destroy(other);
+        }
+    }
+}
